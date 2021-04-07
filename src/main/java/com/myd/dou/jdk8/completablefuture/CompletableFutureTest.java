c7e8013c62210a8360b2f0e9468d3d6c395c2cb0
OF3787 <mayuanding@qianmi.com>
2021-04-07 13:57:44 +0800

    jdk8 completableFuture异步编程

--- src/main/java/com/myd/dou/jdk8/completablefuture/CompletableFutureTest.java
+++ src/main/java/com/myd/dou/jdk8/completablefuture/CompletableFutureTest.java
@@ -1,8 +1,8 @@
 package com.myd.dou.jdk8.completablefuture;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -10,6 +10,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @version 0.1.0
  * @Date:2021/4/7 11:06
  * @Description: deadLock test
+ * https://www.cnblogs.com/lyuweigh/p/9568697.html
+ * https://www.jianshu.com/p/6f3ee90ab7d3 还有很多其他特性
  */
 public class CompletableFutureTest {
 
@@ -25,7 +27,28 @@ public class CompletableFutureTest {
         }
     });
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<CompletableFuture<Void>> list = new ArrayList<>();
 
+        for (int i=0;i<3;i++){
+            CompletableFuture<Void> fu = CompletableFuture.runAsync(()->{
+                try {
+                    Thread.sleep(000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                CompletableFuture<Void> child = CompletableFuture.runAsync(()->{
+                    System.out.println("我被执行了啊");
+                },POOL);
+                child.join();
+
+            },POOL);
+
+            System.out.println(String.format("第%s个父任务等待完成",i));
+
+            list.add(fu);
+        }
+        System.out.println(list.get(0).get());
     }
 }
