bde37e2d9671dee7439010bc34cb4ebac4a9f969
OF3787 <mayuanding@qianmi.com>
2021-05-28 14:02:00 +0800

    接口参数化

--- src/main/java/com/myd/dou/jdk8/lambda/supplier/TestMain.java
+++ src/main/java/com/myd/dou/jdk8/lambda/supplier/TestMain.java
@@ -1,12 +1,14 @@
 package com.myd.dou.jdk8.lambda.supplier;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="mailto:mayuanding@qianmi.com">OF3787-马元丁</a>
  * @version 0.1.0
  * @Date:2021/4/28 10:26
- * @Description: TODO
+ * @Description:
  */
 public class TestMain {
     public static void main(String[] args) {
@@ -14,5 +16,11 @@ public class TestMain {
         CacheUtil cacheUtil = new CacheUtil();
         String result = cacheUtil.get("B", redisSupplier );
         System.out.println(result);
+
+        String str = "ndjksnkdk";
+        String join = StringUtils.join(str);
+        System.out.println(join);
+
+
     }
 }
